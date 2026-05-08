08c47a2e742d10b3f5189d3fd859f7840775d46b
donneko <117331095+donneko@users.noreply.github.com>
2026-05-08 22:24:08 +0900

    openBrowserにメッセージを追加

--- src/service/open-browser.ts
+++ src/service/open-browser.ts
@@ -1,6 +1,7 @@
 import open from "open";
 import { getLanIp } from "../util/get-lan-ip.js";
 import type { BrowserOpenConfig } from "../types/config.type.js"
+import { logger } from "../util/logger.js";
 
 type OpenBrowserData = {
     host:string;
@@ -18,6 +19,9 @@ export async function openBrowser(openBrowserData:OpenBrowserData):Promise<void>
     const isLAN = host === "0.0.0.0";
     const ip = getLanIp();
     const networkUrl = `http://${ip}:${port}`;
+
+    if((!isLAN && target === "network")) logger.warn("ネットワークが有効ではないため開けません。")
+
     const targetUrl = (isLAN && target === "network")
         ? networkUrl
         : `http://localhost:${port}`;
