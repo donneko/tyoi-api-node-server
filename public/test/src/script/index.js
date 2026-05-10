447f13c6fe8439cd2e2f33d3ff03c5d596863674
donneko <117331095+donneko@users.noreply.github.com>
2026-05-10 21:15:37 +0900

    index.htmlを変更

---
+++ public/test/src/script/index.js
@@ -0,0 +1,7 @@
+const SERVER_STATUS_TABLE   = document.querySelector(".js-server-status-table");
+const URLS_TABLE            = document.querySelector(".js-urls-table");
+const FEATURES_TABLE        = document.querySelector(".js-features-table");
+const ROUTE_LINKS_TABLE     = document.querySelector(".js-route-links-table");
+
+const API_PREFIX = "/api";
+
