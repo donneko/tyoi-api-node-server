d0fe5f49baf0a4e05c4044b3f9df6a7ca9888746
donneko <117331095+donneko@users.noreply.github.com>
2026-05-23 00:01:36 +0900

    defineConfig の import と publicDirname を修正。

--- src/templates/basic/tyoi.config.js
+++ src/templates/basic/tyoi.config.js
@@ -1,10 +1,10 @@
-import { Server } from "@donneko/tyoi-server";
+import { defineConfig } from "@donneko/tyoi-server";
 
 export default defineConfig({
     port: 3000,
     autoPort:true,
 
-    publicDirname:"../public/main",
+    publicDirname:"./public/main",
     apiPrefix: "/api",
 
     exposeLan: false,
