00683ce75a5da79a4478261ea5e3dda918b4d90f
donneko <117331095+donneko@users.noreply.github.com>
2026-06-05 18:50:57 +0900

    cliのコピーをコピー結果表示に対応させた

--- src/cli/create/service/copy-template.ts
+++ src/cli/create/service/copy-template.ts
@@ -1,15 +1,23 @@
+import { logger } from "../../../util/logger.js";
+import { copyFolder } from "./copy-folder.js";
 import fs from "node:fs"
 
-export function copyTemplate(
+export async function copyTemplate(
     templatePath:string,
     projectPath:string
-):void{
-
+):Promise<void>{
     if (fs.existsSync(projectPath)) {
         throw Error(`エラー: コピー先がすでに存在するため、安全のために終了します。 (${projectPath})`);
     }
 
-    fs.cpSync(templatePath, projectPath, {
-        recursive: true
-    });
+    fs.mkdirSync(templatePath);
+
+    const {error,ok}= await copyFolder(templatePath,projectPath);
+    logger.window({
+        title:"コピー結果",
+        content:[
+            ...error.map(m=>logger.createError(m)),
+            ...ok.map(m=>logger.createSuccess(m))
+        ]
+    })
 }
