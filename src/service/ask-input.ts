26c97534e068257ee4a8ec7ae6c35da568f63b8e
donneko <117331095+donneko@users.noreply.github.com>
2026-06-05 19:02:54 +0900

    聞いたあとに、聞いたときのテキストを消去するようにした

--- src/service/ask-input.ts
+++ src/service/ask-input.ts
@@ -1,4 +1,5 @@
 import readline from 'node:readline/promises';
+import rend from "readline";
 import { stdin as input, stdout as output } from 'node:process';
 
 export async function askInput(
@@ -8,9 +9,14 @@ export async function askInput(
     const rl = readline.createInterface({ input, output });
     let answer;
     try {
-        answer = await rl.question(message)
+        answer = await rl.question(message);
+
+        rend.moveCursor(process.stdout, 0,-1);
+        rend.clearLine(process.stdout,0);
+
     } finally {
         rl.close();
+
         return (!answer || answer.length === 0)?
             null:
             answer
