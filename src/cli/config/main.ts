b2abfbb0ae4e59e53b9d28e6994fdc115e94a614
donneko <117331095+donneko@users.noreply.github.com>
2026-06-05 10:48:10 +0900

    serverConfig のフォルダー系作成

---
+++ src/cli/config/main.ts
@@ -0,0 +1,40 @@
+import type { MainContextData } from "../../main.js";
+import { getTemplatePath } from "./service/template.path.js";
+import { getProjectName } from "./service/project-name.js";
+import { fixPath } from "./service/fix-path.js";
+import { copyTemplate } from "./service/copy-template.js";
+import { replacePackageJson } from "./service/replace-name.js";
+import { showNextSteps } from "./service/next-steps.js";
+
+
+export default async function serverConfig(mainContextData:MainContextData){
+
+    const mainDirname = mainContextData.mainDirname;
+    const processCwd = mainContextData.processCwd
+
+    const templatePass = await getTemplatePath(
+        mainContextData.optionArgs?.template,
+        mainDirname
+    );
+
+    const projectName = await getProjectName(
+        mainContextData.commandArgs[1],
+        processCwd
+    );
+
+    const templatePath = fixPath(mainDirname,templatePass);
+    const projectPath  = processCwd;
+
+
+    await copyTemplate(
+        templatePath,
+        projectPath
+    );
+
+    replacePackageJson(
+        projectPath,
+        projectName
+    );
+
+    showNextSteps(projectName);
+}
---
+++ src/cli/config/service/project-name.ts
@@ -0,0 +1,28 @@
+import path from "node:path";
+import { askInput } from "../../../service/ask-input.js";
+import { logger } from "../../../util/logger.js";
+
+export async function getProjectName(
+    argProjectName:string | undefined,
+    processCwd:string
+):Promise<string>{
+
+    let projectName = argProjectName;
+
+    if(!projectName){
+        const defaultName = path.basename(processCwd);
+        const InputProjectName = await askInput(
+                `Project名を入力してください。デフォルト(${defaultName}) : `
+            ) ?? defaultName;
+
+        projectName = InputProjectName
+    }
+
+    if (!/^[a-zA-Z0-9-]+$/.test(projectName)) {
+        throw Error(`プロジェクト名に使える文字は英数字・- のみです。[${projectName}]`);
+    }
+
+    logger.info(`選択されたプロジェクト名[${projectName}]`);
+    return projectName;
+}
+
