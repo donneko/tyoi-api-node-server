e4a081c479900b1fffe5b3c23a467d9d5eaa0841
donneko <117331095+donneko@users.noreply.github.com>
2026-06-07 00:49:57 +0900

    helpコマンドの最新化

--- src/cli/command/help.ts
+++ src/cli/command/help.ts
@@ -2,12 +2,19 @@ import type { CmdMetaData } from "../main.js";
 import { logger } from "../../util/logger.js";
 
 export default function serverHelp(data:CmdMetaData){
-    logger.bar();
-    logger.info("HELP : tyoi-server-cli");
-    logger.info(`更新[2026/06/04]:現在使用できるコマンドは以下の通りです。`);
-    logger.message(`dev  : 動作確認用の設定で動作確認用サーバーが起動します。`);
-    logger.message(`help : 使用できるコマンド一覧が確認できます。`);
-    logger.message(`init : 現在いるディレクトリーにテンプレートをコピーします。`);
-    logger.message(`run  : 現在のディレクトリーのサーバーを起動します`);
-    logger.bar();
+
+    logger.window({
+        title:"help 表示",
+        content:[
+            logger.createInfo(`現在使用できるコマンドは以下の通りです。\n更新 : [2026/06/07]`),
+            logger.createBar(),
+            logger.createMessage(`dev    : 動作確認用の設定で動作確認用サーバーが起動します。`),
+            logger.createMessage(`help   : 使用できるコマンド一覧が確認できます。`),
+            logger.createMessage(`init   : 現在いるディレクトリーにサーバーを作成します`),
+            logger.createMessage(`run    : 現在のディレクトリーのサーバーを起動します`),
+            logger.createMessage(`create : 新しいフォルダーにサーバーを作成します`),
+            logger.createMessage(`config : 現在のディレクトリーに設定ファイルを追加します`),
+            logger.createMessage(`info   : "tyoi run" の設定が適用されるの確認結果が表示されます`)
+        ]
+    });
 }
