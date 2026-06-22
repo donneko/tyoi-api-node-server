e0b73b1f55b0cfb1f0b1663301c2ebb56acf7f39
donneko <117331095+donneko@users.noreply.github.com>
2026-06-22 21:25:31 +0900

    eslint 設定の編集

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,16 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
     {
-        ignores: ["dist", "coverage", "docs", "node_modules"],
+        ignores: ["dist", "coverage", "docs", "node_modules", "package-lock.json", ".github"],
     },
 
     js.configs.recommended,
     ...tseslint.configs.recommended,
+    eslintConfigPrettier,
 
     {
         files: ["**/*.{js,ts}"],
@@ -22,15 +24,8 @@ export default tseslint.config(
         rules: {
             "no-console": "off",
             "prefer-const": "warn",
-
             "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": [
-                "warn",
-                {
-                    argsIgnorePattern: "^_",
-                    varsIgnorePattern: "^_",
-                },
-            ],
+            "@typescript-eslint/no-unused-vars": "warn",
         },
     }
 );
