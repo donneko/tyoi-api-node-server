7b94bc51fa6c2a413281c7e70a844ebe186f3e0f
donneko <117331095+donneko@users.noreply.github.com>
2026-06-24 15:58:16 +0900

    #25

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Server } from "./app/server.js";
-export { tyoi } from "./app/short-handler.js";
-export { defineConfig } from "./app/config-server.js";
+export { Server } from "./server/app/server.js";
+export { tyoi } from "./server/app/short-handler.js";
+export { defineConfig } from "./server/app/config-server.js";
